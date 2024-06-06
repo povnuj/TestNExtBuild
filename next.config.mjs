a19d23c99954a7ea68fa7e5ded1c0fccf4954782
M.Losyk <Mykola.Losyk@gmail.com>
2024-06-06 15:26:01 +0300

    test6.11

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,34 @@
 /** @type {import('next').NextConfig} */
-import CompressionPlugin from 'compression-webpack-plugin';
+//import CompressionPlugin from 'compression-webpack-plugin';
 
 const nextConfig = {
   swcMinify: true,
  
   output: 'standalone',
   //distDir: 'dist',
- // output: 'export',
+  //output: 'export',
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'diameb.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'test-og-next.netlify.app',
-        pathname: '/**',
-      },
-    ],
-    formats: ['image/avif', 'image/webp'],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'diameb.com',
+  //       pathname: '/**',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'test-og-next.netlify.app',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  //   formats: ['image/avif', 'image/webp'],
+  // },
 
   // webpack: (config, { isServer }) => {
   //   if (!isServer) {
   //     config.resolve.fallback.fs = false; // Виключення модуля 'fs' на клієнтській стороні
   //   }
-
-  //   // Додавання компресії Brotli
+  // // Додавання компресії Brotli
   //   config.plugins.push(new CompressionPlugin({
   //     filename: '[path][base].br',
   //     algorithm: 'brotliCompress',
@@ -39,41 +38,40 @@ const nextConfig = {
   //     minRatio: 0.8,
   //     deleteOriginalAssets: false,
   //   }));
-
-  //   return config;
+  // return config;
   // },
   
 
  // Оптимізація кешування і служби доставки контенту
-  async headers() {
-   return [
-     {
-       source: '/(.*)',
-       headers: [
-         {
-           key: 'Cache-Control',
-           value: 'public, max-age=31536000, immutable', 
-         },
-         {
-           key: 'Content-Security-Policy',
-           value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'", // Політика безпеки контенту
-         },
-         {
-           key: 'X-Content-Type-Options',
-           value: 'nosniff',
-         },
-         {
-           key: 'X-Frame-Options',
-           value: 'DENY',
-         },
-         {
-           key: 'X-XSS-Protection',
-           value: '1; mode=block',
-         },
-       ],
-     },
-   ];
-  },
+  // async headers() {
+  //  return [
+  //    {
+  //      source: '/(.*)',
+  //      headers: [
+  //        {
+  //          key: 'Cache-Control',
+  //          value: 'public, max-age=604800, immutable', 
+  //        },
+  //        {
+  //          key: 'Content-Security-Policy',
+  //          value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'", // Політика безпеки контенту
+  //        },
+  //        {
+  //          key: 'X-Content-Type-Options',
+  //          value: 'nosniff',
+  //        },
+  //        {
+  //          key: 'X-Frame-Options',
+  //          value: 'DENY',
+  //        },
+  //        {
+  //          key: 'X-XSS-Protection',
+  //          value: '1; mode=block',
+  //        },
+  //      ],
+  //    },
+  //  ];
+  // },
   env: {
     
   },
