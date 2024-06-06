42758e3f39b20e4b5d00075f6561adba910a9ee4
M.Losyk <Mykola.Losyk@gmail.com>
2024-06-06 14:00:41 +0300

    test6.4

--- next.config.mjs
+++ next.config.mjs
@@ -54,10 +54,10 @@ const nextConfig = {
            key: 'Cache-Control',
            value: 'public, max-age=31536000, immutable', 
          },
-         {
-           key: 'Content-Security-Policy',
-           value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'", // Політика безпеки контенту
-         },
+        //  {
+        //    key: 'Content-Security-Policy',
+        //    value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'", 
+        //  },
          {
            key: 'X-Content-Type-Options',
            value: 'nosniff',
