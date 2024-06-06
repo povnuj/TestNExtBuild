e7a14cbf1b3c91c099cb8cae164a3f20ef627074
M.Losyk <Mykola.Losyk@gmail.com>
2024-06-06 14:03:21 +0300

    test6.5

--- next.config.mjs
+++ next.config.mjs
@@ -58,18 +58,18 @@ const nextConfig = {
         //    key: 'Content-Security-Policy',
         //    value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'", 
         //  },
-         {
-           key: 'X-Content-Type-Options',
-           value: 'nosniff',
-         },
-         {
-           key: 'X-Frame-Options',
-           value: 'DENY',
-         },
-         {
-           key: 'X-XSS-Protection',
-           value: '1; mode=block',
-         },
+        //  {
+        //    key: 'X-Content-Type-Options',
+        //    value: 'nosniff',
+        //  },
+        //  {
+        //    key: 'X-Frame-Options',
+        //    value: 'DENY',
+        //  },
+        //  {
+        //    key: 'X-XSS-Protection',
+        //    value: '1; mode=block',
+        //  },
        ],
      },
    ];
